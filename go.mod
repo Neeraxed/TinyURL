6ee9f9bf66ca4447880cacb1b8752b9b0e83b11e
Neeraxed <neeraxed@gmail.com>
2024-10-14 20:11:42 +0300

    lil refactor

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23.2
 require github.com/sqids/sqids-go v0.4.1
 
 require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	github.com/teris-io/shortid v0.0.0-20220617161101-71ec9f2aa569 // indirect
 )
--- test.go
+++ test.go
@@ -2,85 +2,107 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 
+	"github.com/julienschmidt/httprouter"
 	_ "github.com/lib/pq" //To import a package solely for its side-effects (initialization), use the blank identifier as explicit package name - хз вообще что значит
 	"github.com/teris-io/shortid"
 )
 
-var database *sql.DB
+type Usecase struct {
+	connStr  string
+	database *sql.DB
+}
 
-func main() {
-	//Database connection
-	connStr := "user=tester password=tester dbname=tester sslmode=disable"
-	db, err := sql.Open("postgres", connStr)
+func (usecase *Usecase) Init() {
+	usecase.connStr = "user=tester password=tester dbname=tester sslmode=disable"
+
+	db, err := sql.Open("postgres", usecase.connStr)
 	if err != nil {
 		panic(err)
 	}
-	database = db // хз зачем (почему database, err := sql.Open(...) не работало), надо разобраться
-	defer db.Close()
+	usecase.database = db
+	defer usecase.database.Close()
+}
 
-	//HTTP server
-	http.HandleFunc("/", handler)
-	err1 := http.ListenAndServe(":3333", nil)
-	if err1 != nil {
-		fmt.Println("ашипка: ", err1)
+func (usecase *Usecase) indexGet(w http.ResponseWriter, r *http.Request) {
+
+	bytesBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("bad request body"))
+		return
+	}
+
+	recievedLink := (string)(bytesBody)
+	link, err := usecase.GetFromDB(recievedLink)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	http.Redirect(w, r, link, http.StatusSeeOther)
+}
+
+func (usecase *Usecase) indexPost(w http.ResponseWriter, r *http.Request) {
+
+	bytesBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err)
+		w.Write([]byte("Bad request body"))
+		return
+	}
+	recievedLink := (string)(bytesBody)
+
+	id, ok := shortid.Generate()
+	if ok != nil {
+		log.Println("Did not generate")
+	}
+	id = "/" + id
+
+	link, err := usecase.GetFromDB(recievedLink)
+	if err == nil {
+		log.Println("Existing link")
+		w.Write([]byte(link))
+		return
+	}
+
+	usecase.AddToDB(id, recievedLink)
+}
+
+type ConnectDB struct {
+	usecase *Usecase
+}
+
+func (usecase *Usecase) AddToDB(id, recievedLink string) {
+
+	_, err := usecase.database.Exec("insert into links (short_link, long_link) values ($1, $2)",
+		id, recievedLink)
+	if err != nil {
+		panic(err)
 	}
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (usecase *Usecase) GetFromDB(long_link string) (string, error) {
 
-	if r.Method == "POST" {
-
-		bytesBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Println(err)
-			w.Write([]byte("bad request body"))
-			return
-		}
-		recievedLink := (string)(bytesBody)
-
-		//Check if link already exists
-		var link string
-		err = database.QueryRow("select short_link from links where long_link = $1", recievedLink).Scan(&link)
-		if err == nil {
-			log.Println("existing link")
-			w.Write([]byte(link))
-			return
-		}
-
-		id, ok := shortid.Generate()
-		if ok != nil {
-			fmt.Println("did not generate")
-		}
-		id = "/" + id
-
-		w.Write([]byte(id))
-
-		//Database write
-		_, err = database.Exec("insert into links (short_link, long_link) values ($1, $2)",
-			id, recievedLink)
-		if err != nil {
-			panic(err)
-		}
-
-	} else if r.Method == "GET" {
-
-		//Database read
-		var link string
-		err := database.QueryRow("select long_link from links where short_link = $1", r.URL.Path).Scan(&link)
-		if err != nil {
-			log.Println(err)
-		}
-
-		http.Redirect(w, r, link, http.StatusSeeOther)
-
-	} else {
-
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("неправильный метод запроса"))
+	var link string
+	error := usecase.database.QueryRow("select short_link from links where long_link = $1", long_link).Scan(&link)
+	if error != nil {
+		log.Println(error)
 	}
+	return link, error
+}
+
+func main() {
+	usecase := Usecase{}
+	usecase.Init()
+
+	router := httprouter.New()
+	router.HandlerFunc("GET", "/", usecase.indexGet)
+	router.HandlerFunc("POST", "/", usecase.indexPost)
+
+	err := http.ListenAndServe(":3333", nil)
+	log.Fatal(err)
 }
